f6caf7676ef6674bfa768404b4d414b877d94fcd
Asha Paul <ashajpaul@gmail.com>
2022-02-17 17:54:19 -0500

    fix next bill date for yearly cycle

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -17,7 +17,69 @@ import {
 
 const Subscription = ({ subscription, onEditClick, deleteSubscription }) => {
 
+  let gapi = window.gapi
+  let CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+  let API_KEY = process.env.REACT_APP_CALENDAR_API_KEY
+  let DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+  let SCOPES = ['https://www.googleapis.com/auth/analytics.manage.users'];
 
+  const addCalendarEvent = (startTime, address, clientName) => {
+    gapi.load('client:auth2', () => {
+      console.log('loaded client')
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+
+      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+      // time zone list:
+      // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+      let timeZone = "America/New_York";
+
+      
+      //sign in with pop up window
+      gapi.auth2.getAuthInstance().signIn()
+        console.log(gapi.auth2.getAuthInstance())
+        .then(() => {
+
+          let event = {
+            'summary': clientName, // or event name
+            'location': address, //where it would happen
+            'start': {
+              'dateTime': subscription.date,
+              'timeZone': timeZone
+            },
+            'end': {
+              'dateTime': subscription.date,
+              'timeZone': timeZone
+            },
+            'recurrence': [
+              'RRULE:FREQ=DAILY;COUNT=1'
+            ],
+            'reminders': {
+              'useDefault': false,
+              'overrides': [
+                { 'method': 'popup', 'minutes': 20 }
+              ]
+            }
+          }
+
+          let request = gapi.client.calendar.events.insert({
+            'calendarId': 'primary',
+            'resource': event,
+          })
+
+          request.execute(event => {
+            console.log(event)
+            window.open(event.htmlLink)
+          })
+
+        })
+    })
+  }
 
   const nextBill = (cycle, date) => {
     const startDate = moment.unix(date.seconds)
@@ -25,11 +87,12 @@ const Subscription = ({ subscription, onEditClick, deleteSubscription }) => {
     let nextDate;
 
     if (cycle === "yearly") {
-      const diff = moment().diff(date, "years", true);
-      nextDate =
-        diff > 0
-          ? startDate.add(Math.floor(diff) + 1, "year")
-          : startDate
+      if (startDate.isBefore(now)) {
+        const diff = now.diff(startDate, "years");
+        nextDate = startDate.add(diff + 1, "year")
+      } else {
+        nextDate = startDate
+      }
     } else if (cycle === "monthly") {
       if (startDate.isBefore(now)) {
         const diff = now.diff(startDate, "months");
@@ -60,7 +123,7 @@ const Subscription = ({ subscription, onEditClick, deleteSubscription }) => {
       <td>{nextBill(subscription.cycle, subscription.date)}</td>
       <td className="sub__deleteNedit">
 
-        <button color="info" className="btn btn-outline-info" >
+        <button color="info" className="btn btn-outline-info" onClick={addCalendarEvent}>
           <MdNotificationAdd />
         </button>
         <button color="info" className="btn btn-outline-info" onClick={() => onEditClick(subscription)}>
